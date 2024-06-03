e75e902d320ccfe0559c60ee63ed7e7835006e2c
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-06-03 19:03:29 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,14 @@
+p1 = input("valor 1: ")
+p2 = input("v2: ")
+
+if p1 > p2:
+    print("valor 1 é maior ou igual que valor 2")
+else:
+    print("valor 2 é maior ou igual que valor 1")
+
+
+
+
+
+
+#python py.py
